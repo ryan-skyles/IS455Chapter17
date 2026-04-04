3db33f09c883f2632ae615cfca2a00c9e29fabe1
Colin Pettry <colin.pettry@gmail.com>
2026-04-04 16:21:28 -0600

    fixed button error

--- web/app/(shop)/orders/[orderId]/page.tsx
+++ web/app/(shop)/orders/[orderId]/page.tsx
@@ -11,7 +11,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/lib/button-variants";
 import { cn } from "@/lib/utils";
 
 type Props = { params: Promise<{ orderId: string }> };
--- web/components/Nav.tsx
+++ web/components/Nav.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/lib/button-variants";
 import { cn } from "@/lib/utils";
 
 type Props = { customerLabel: string | null };
